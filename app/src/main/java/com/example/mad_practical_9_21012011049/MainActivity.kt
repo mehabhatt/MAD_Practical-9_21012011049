8c8df8f74f6e0b6a03bc00aecd443e0aed048afb
mehabhatt <mehabhatt21@gmail.com>
2023-10-26 23:55:16 +0530

    Practical 9

--- app/src/main/java/com/example/mad_practical_9_21012011049/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012011049/MainActivity.kt
@@ -1,36 +1,33 @@
 package com.example.mad_practical_9_21012011049
-
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var  alarmanimation : AnimationDrawable
-    lateinit var  heartanimation : AnimationDrawable
+    lateinit var alarmanimation : AnimationDrawable
+    lateinit var heartanimation : AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val image: ImageView = findViewById(R.id.imageView)
-        image.setBackgroundResource(R.drawable.alarm_animation_list)
-        alarmanimation=image.background as AnimationDrawable
-        val heart: ImageView = findViewById(R.id.heartImages)
-        heart.setBackgroundResource(R.drawable.heart_animation_list)
-        heartanimation= heart.background as AnimationDrawable
+        val img = findViewById<ImageView>(R.id.imageView)
+        img.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation = img.background as AnimationDrawable
+        val animate = findViewById<ImageView>(R.id.heartImages)
+        animate.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation = animate.background as AnimationDrawable
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
+        if (hasFocus){
             alarmanimation.start()
             heartanimation.start()
         }
-        else
-        {
-           alarmanimation.stop()
+        else{
+            alarmanimation.stop()
             heartanimation.stop()
         }
-
     }
 
 
--- app/src/main/java/com/example/mad_practical_9_21012011049/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012011049/SplashActivity.kt
@@ -1,52 +1,44 @@
 package com.example.mad_practical_9_21012011049
-
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
 import android.widget.ImageView
 import android.view.animation.AnimationUtils
-import android.view.animation.Animation.AnimationListener
-
 
 class SplashActivity : AppCompatActivity(), AnimationListener {
-    lateinit var logoanimation: AnimationDrawable
+    lateinit var uvpcelogoanimation : AnimationDrawable
     lateinit var twinAnimation: Animation
-    lateinit var image: ImageView
+    lateinit var img: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val image: ImageView = findViewById(R.id.uvpce_logo)
-        image.setBackgroundResource(R.drawable.uvpce_animation_list)
-        logoanimation = image.background as AnimationDrawable
+        img = findViewById<ImageView>(R.id.uvpce_logo)
+        img.setBackgroundResource(R.drawable.uvpce_animation_list)
+        uvpcelogoanimation = img.background as AnimationDrawable
         twinAnimation = AnimationUtils.loadAnimation(this, R.anim.twin_animation)
         twinAnimation.setAnimationListener(this)
     }
-
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            logoanimation.start()
-            image.startAnimation(twinAnimation)
-        } else {
-            logoanimation.stop()
+        if (hasFocus){
+            uvpcelogoanimation.start()
+            img.startAnimation(twinAnimation)
         }
-
+        else{
+            uvpcelogoanimation.stop()
+        }
     }
-
     override fun onAnimationStart(p0: Animation?) {
 
-
     }
-
     override fun onAnimationEnd(p0: Animation?) {
-        Intent(this, MainActivity::class.java).also { startActivity(it) }
+        Intent(this,MainActivity::class.java).apply { startActivity(this) }
+    }
+    override fun onAnimationRepeat(p0: Animation?){
 
     }
 
-    override fun onAnimationRepeat(p0: Animation?) {
-
-    }
 }
-
